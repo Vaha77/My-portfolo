9fd46b3d59a6525c4ec20f5bce139a69848ab051
vaha77 <abdulvohid996@gmail.com>
2022-02-25 04:51:27 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import About from "./Components/About";
 import Card from "./Components/Card";
 function App() {
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
       <Card />
+      <About />
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div className="max-w-4xl mt-20 mx-auto">
+        <p className="text-2xl text-black  md:text-4xl font-bold text-center">
+          Hey 👋
+        </p>
+        <p className="text-base text-left md:text-center text-gray-500 leading-relaxed mt-4 justify-center">
+          I am a programmer with hands-on experience in advanced web
+          development. Equipped with advanced and latest software skills in tech
+          world. Passionate to learn new tech skills and improve. Extroverted
+          and eager to learn from colleges and build a friendly environment.
+          Adept in various social media platforms and office technology
+          programs.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
