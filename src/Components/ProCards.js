56bc96c30e426088209ae2f4196a9b6afa8ac68f
vaha77 <abdulvohid996@gmail.com>
2022-02-25 05:58:56 +0900

    updada

--- src/Components/ProCards.js
+++ src/Components/ProCards.js
@@ -41,6 +41,11 @@ function ProCards() {
       description: "Real-time website using real-time API to fetch data from",
       link: "https://eager-morse-9c95b7.netlify.app",
     },
+    {
+      name: "Eager",
+      description: "Real-time website using real-time API to fetch data from",
+      link: "https://eager-morse-9c95b7.netlify.app",
+    },
   ]);
 
   return (
